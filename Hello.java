2833a6f4e385ded48dfd54ff245a595979d3ce24
BaKhoat Cao <72607418+Caobakhoat@users.noreply.github.com>
2020-10-13 21:02:34 +0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import jdk.internal.vm.PostVMInitHook;
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class Hello {
+    public static void main(){
+        System.out.println("Hello team");
+    }
+}
